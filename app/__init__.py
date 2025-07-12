47f19f0c192e67d5a31e79c7e6a0be9b922a2656
Taro Anzai <taro.anzai@gmail.com>
2025-07-13 01:30:18 +0900

    update to add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from dotenv import load_dotenv
+
+
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(config_class=Config):
+    load_dotenv()
+
     app = Flask(__name__)
     app.config.from_object(config_class)
 
@@ -16,6 +21,35 @@ def create_app(config_class=Config):
 
     # モデル登録
     from . import models
+
+    #Blueprint登録
+    from app.routes.access_scope_routes import access_scope_bp
+    from app.routes.ai_route import ai_bp
+    from app.routes.auth_routes import auth_bp
+    from app.routes.company_routes import company_bp
+    from app.routes.objectives_route import objectives_bp
+    from app.routes.organization_routes import organization_bp
+    from app.routes.progress_updates_route import progress_bp
+    from app.routes.task_access_route import task_access_bp
+    from app.routes.task_core_route import task_core_bp
+    from app.routes.task_export_route import task_export_bp
+    from app.routes.task_order_route import task_order_bp
+    from app.routes.test_routes import test_bp
+    from app.routes.user_routes import user_bp
+
+    app.register_blueprint(access_scope_bp)
+    app.register_blueprint(ai_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(company_bp)
+    app.register_blueprint(objectives_bp)
+    app.register_blueprint(organization_bp)
+    app.register_blueprint(progress_bp)
+    app.register_blueprint(task_access_bp)
+    app.register_blueprint(task_core_bp)
+    app.register_blueprint(task_export_bp)
+    app.register_blueprint(task_order_bp)
+    app.register_blueprint(test_bp)
+    app.register_blueprint(user_bp)
 
     return app
 
